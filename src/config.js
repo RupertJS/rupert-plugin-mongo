0a572ca4d8958676cbccb711b5743ac54cf86d75
David Souther <davidsouther@gmail.com>
2015-06-12 08:06:24 -0400

    Expose getDatabase for consumers.

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 module.exports = function (config){
   config.prepend('routing', __dirname + '/mongo/mongo-route.js');
 };
+
+module.exports.getDatabase = require('./mongo/mongo-promise').getDatabase;
